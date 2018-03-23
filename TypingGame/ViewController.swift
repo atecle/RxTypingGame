1204fed8d81a7de239702c317955df3637fed6f6
atecle <adam.tecle@gc.com>
2018-03-23 09:24:48 -0400

    Initial Commit

---
+++ TypingGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypingGame
+//
+//  Created by adam tecle on 3/23/18.
+//  Copyright © 2018 adam tecle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
